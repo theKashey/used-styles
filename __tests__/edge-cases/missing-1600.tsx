986982704f65e512eb43c59f3c45357bb463eca7
Anton Korzunov <thekashey@gmail.com>
2021-09-19 16:46:22 +1000

    add edge case for #31

---
+++ __tests__/edge-cases/missing-1600.tsx
@@ -0,0 +1,47 @@
+import { buildAst } from '../../src/parser/toAst';
+import { fromAst } from '../../src/parser/fromAst';
+import { createUsedFilter } from '../../src/utils/cache';
+
+describe('missing 1600px', () => {
+  const CSS = `
+.content {    
+}
+
+@media screen and (min-width: 768px) {
+    .content {
+        grid-column:1/6
+    }
+}
+
+@media screen and (min-width: 1024px) {
+    .content {
+        grid-column:1/7
+    }
+}
+
+/*this style duplicates 768 one, had the hash before*/
+@media screen and (min-width: 1600px) {
+    .content {
+        grid-column:1/6
+    }
+}`;
+
+  it('result should contain 1600', () => {
+    const ast = buildAst(CSS);
+
+    const test = fromAst(['content'], ast, createUsedFilter());
+    expect(test).toMatchInlineSnapshot(`
+      ".content {  }
+
+      @media screen and (min-width: 768px) {
+      .content { grid-column: 1/6; }
+      }
+      @media screen and (min-width: 1024px) {
+      .content { grid-column: 1/7; }
+      }
+      @media screen and (min-width: 1600px) {
+      .content { grid-column: 1/6; }
+      }"
+    `);
+  });
+});
