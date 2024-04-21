82ec22a39e9e747f4f916bade45a010e1cb20d4a
Ivan Badicean <badican01117@gmail.com>
2024-04-21 12:36:06 +0300

    Added react vite templates

---
+++ example/ssr-react-streaming-ts/server.js
@@ -0,0 +1,100 @@
+import fs from 'node:fs/promises'
+import express from 'express'
+
+// Constants
+const isProduction = process.env.NODE_ENV === 'production'
+const port = process.env.PORT || 5173
+const base = process.env.BASE || '/'
+const ABORT_DELAY = 10000
+
+// Cached production assets
+const templateHtml = isProduction
+  ? await fs.readFile('./dist/client/index.html', 'utf-8')
+  : ''
+const ssrManifest = isProduction
+  ? await fs.readFile('./dist/client/.vite/ssr-manifest.json', 'utf-8')
+  : undefined
+
+// Create http server
+const app = express()
+
+// Add Vite or respective production middlewares
+let vite
+if (!isProduction) {
+  const { createServer } = await import('vite')
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+    base
+  })
+  app.use(vite.middlewares)
+} else {
+  const compression = (await import('compression')).default
+  const sirv = (await import('sirv')).default
+  app.use(compression())
+  app.use(base, sirv('./dist/client', { extensions: [] }))
+}
+
+// Serve HTML
+app.use('*', async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, '')
+
+    let template
+    let render
+    if (!isProduction) {
+      // Always read fresh template in development
+      template = await fs.readFile('./index.html', 'utf-8')
+      template = await vite.transformIndexHtml(url, template)
+      render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render
+    } else {
+      template = templateHtml
+      render = (await import('./dist/server/entry-server.js')).render
+    }
+
+    let didError = false
+
+    const { pipe, abort } = render(url, ssrManifest, {
+      onShellError() {
+        res.status(500)
+        res.set({ 'Content-Type': 'text/html' })
+        res.send('<h1>Something went wrong</h1>')
+      },
+      onAllReady() {
+        res.status(didError ? 500 : 200)
+        res.set({ 'Content-Type': 'text/html' })
+
+        let [htmlStart, htmlEnd] = template.split(`<!--app-html-->`)
+
+        // React 19 supports document metadata out of box, 
+        // but for react 18 we can use `react-helmet-async` here:
+        // htmlStart = htmlStart.replace(`<!--app-head-->`, helmet.title.toString())
+
+        res.write(htmlStart)
+        pipe(res)
+        res.write(htmlEnd)
+
+        res.end()
+      },
+      onError(error) {
+        didError = true
+        console.error(error)
+        // You can log crash reports here:
+        // logServerCrashReport(error)
+      }
+    })
+
+    setTimeout(() => {
+      abort()
+    }, ABORT_DELAY)
+  } catch (e) {
+    vite?.ssrFixStacktrace(e)
+    console.log(e.stack)
+    res.status(500).end(e.stack)
+  }
+})
+
+// Start http server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`)
+})
